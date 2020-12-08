80a52c0c7e0ba7a9f4c2bb6dd723baba3de17bc6
Silas Michael Knight <silasfelinus@gmail.com>
2020-12-08 05:45:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,25 @@
 class Triangle
+  attr_accessor :equilateral, :isosceles, :scalene, :unknwon, :side1, :side2, :side3
 
   def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+
   end
+
+  def kind
+    triangle_kind = :unknown
+    if @size1 == @size2 && @size2 == @size3
+      triangle_kind = :equilateral
+    elsif @size1 == @size2 || @size2 == @size3
+      triangle_kind = :isosceles
+    else
+      triangle_kind = :scalene
+    end
+    triangle_kind
+  end
+
 
   class TriangleError < StandardError
     # triangle error code
